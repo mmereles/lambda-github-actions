cb126e8306b8529e217ac7d599256fac30ecb569
mmereles <60193314+mmereles@users.noreply.github.com>
2024-04-02 12:39:45 -0300

    Update main.tf

--- serverless/iac/modules/lambda/main.tf
+++ serverless/iac/modules/lambda/main.tf
@@ -1,24 +1,24 @@
 resource "aws_iam_role" "iam_for_lambda" {
-    name                = "${var.name}-lambda-role"
-    assume-role-policy  = data.aws_iam_policy_document.assume_role.json
+  name               = "${var.name}-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
 resource "aws_lambda_function" "lambda" {
-    filename    = data.archive_file.lambda.output_path
-    function_name = var.name
-    role = aws_iam_role.iam_for_lambda.arn
-    handler = var.handler
-    runtime = "nodejs20.x"
+  filename      = data.archive_file.lambda.output_path
+  function_name = var.name
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = var.handler
+  runtime       = var.runtime
 }
 
 resource "aws_iam_policy" "lambda_logging" {
-    name        = "lambda_logging_${aws_lambda_function.lambda.function_name}"
-    path        = "/"
-    description = "IAM policy for logging from a lambda"
-    policy      = data.aws_iam_policy_document.lambda_logging.json
+  name        = "lambda_logging_${aws_lambda_function.lambda.function_name}"
+  path        = "/"
+  description = "IAM policy for logging from a lambda"
+  policy      = data.aws_iam_policy_document.lambda_logging.json
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_logs" {
-    role        = aws_iam_role.iam_for_lambda.name
-    policy_arn  = aws_iam_policy.lambda_logging_arn
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.lambda_logging.arn
 }

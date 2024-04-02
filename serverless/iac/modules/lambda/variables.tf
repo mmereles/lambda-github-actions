53dacf6ad715e690dec22136db7957c0d9121c80
mmereles <60193314+mmereles@users.noreply.github.com>
2024-04-02 12:40:33 -0300

    Update variables.tf

--- serverless/iac/modules/lambda/variables.tf
+++ serverless/iac/modules/lambda/variables.tf
@@ -1,11 +1,17 @@
 variable "name" {
-    description = "The name of the lambda funtion"
-    type        = string
-    nullable    = false
+  description = "The name of the lambda funtion"
+  type        = string
+  nullable    = false
 }
 
 variable "handler" {
-    description = "the handler function in your code for the lambda function"
-    type        = string
-    default     = "index.handler"
+  description = "the handler function in your code for the lambda function"
+  type        = string
+  default     = "index.handler"
+}
+
+variable "runtime" {
+  description = "The runtime for the Lambda function [nodejs20.x, go1.x]"
+  type        = string
+  default     = "nodejs20.x"
 }
